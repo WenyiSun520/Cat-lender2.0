777d3af3e5597c6051a8b2239db2a11ca84a6d44
wenyi <winnieee.sun@gmail.com>
2023-01-30 01:05:41 -0800

    setup petRecords

--- src/features/petEvents/Calender.js
+++ src/features/petEvents/Calender.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import "./calender.css";
 import {
   faArrowAltCircleLeft,
   faArrowAltCircleRight,
