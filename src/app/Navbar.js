bd83530342665695f446336a90e20e7b6dfac35a
wenyi <winnieee.sun@gmail.com>
2023-01-30 03:55:20 -0800

    add responsive css

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,30 +1,45 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
+export const Navbar = () => {
+  let count = 0;
+  const toggleMenu = () => {
+    if (count % 2 === 0) {
+      document.querySelector("nav").style = "display:block";
+      count++;
+    } else {
+      document.querySelector("nav").style = "display:none";
+      count--;
+    }
+  };
 
-export const Navbar= ()=>{
-    return (
-      <section className="navbar-section">
+  return (
+    <section className="navbar-section">
+      <div className="title">
         <Link to="/" className="nav-link main">
           <h1 className="title">Cat-lender</h1>
         </Link>
-        <nav className="nav nav-pills nav-fill">
-          <div className="nav-item">
-            <Link to="/" className="nav-link">
-              Pets Profiles
-            </Link>
-          </div>
-          <div className="nav-item">
-            <Link to="/calender" className="nav-link nav-item">
-              Pets Calender
-            </Link>
-          </div>
-          <div className="nav-item">
-            <Link to="/records" className="nav-link nav-item">
-              Records(pending)
-            </Link>
-          </div>
-        </nav>
-      </section>
-    );
-}
+        <div className="menu" onClick={toggleMenu}>
+          Menu
+        </div>
+      </div>
+      <nav className="nav nav-pills nav-fill">
+        <div className="nav-item">
+          <Link to="/" className="nav-link">
+            Pets Profiles
+          </Link>
+        </div>
+        <div className="nav-item">
+          <Link to="/calender" className="nav-link nav-item">
+            Pets Calender
+          </Link>
+        </div>
+        <div className="nav-item">
+          <Link to="/records" className="nav-link nav-item">
+            Records(pending)
+          </Link>
+        </div>
+      </nav>
+    </section>
+  );
+};
